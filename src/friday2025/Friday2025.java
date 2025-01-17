9e77978660933ce9522df894460667693e5be0f4
Henil Patel <Henil Patel@KING>
2025-01-17 16:40:22 -0500

    showing other commit

--- src/friday2025/Friday2025.java
+++ src/friday2025/Friday2025.java
@@ -13,8 +13,10 @@ public class Friday2025 {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
        System.out.println("Hi");
+    //showing other commit
     }
     
 }
